5ac38c2f0aab97407bb34744a5a9757a79c132d7
GZhonghui <mail@gzher.com>
2025-04-25 22:06:47 +0900

    增加注释

--- go.mod
+++ go.mod
@@ -1,7 +1,17 @@
+// 如果没有想要代码复用 这里 Module Path 可以随便写
 module local.dev/GoCli
 
+// 语言版本
 go 1.24.2
 
+// 导入的第三方代码 一般是 Github 地址
+// github url / v2 (if version >= 2) / version (tag)
+// 安装（下载）第三方的代码使用 go get
+// go get module_path/(v2)@v2.1.0
+
+// 可以同时导入同一个 module 的多个不同的版本
+// 条件是它们的「大版本」都不同
+// 因为「大版本」不同的话，module path就不同（后面的/v<x>不同）
 require (
 	github.com/GZhonghui/GoMod v1.0.1
 	github.com/GZhonghui/GoMod/v2 v2.1.1
--- main.go
+++ main.go
@@ -1,10 +1,24 @@
+// 在此目录下执行：
+// go run .
+
+// 入口函数必须在这个包里面
+// 目录名：GoCli
+// 包名：main
 package main
 
+// import的对象是package
+// import path都是从module path开始写
 import (
-	gomod "github.com/GZhonghui/GoMod"
+	// 通过使用别名 导入了同一个 module 的两个不同的版本
+	// Package名称冲突的时候 就要使用别名
+	gomod "github.com/GZhonghui/GoMod" // 为 package 定义别名
 	gomodv2 "github.com/GZhonghui/GoMod/v2"
-	"local.dev/GoCli/tools"
+	"local.dev/GoCli/tools" // 这个是本地的 package，写法也是一样的规则
 )
+
+// 上面有从Module Path导入 也有从 Package Path导入
+// 但是本质上 都是导入 Package
+// 也就是说 import 不会递归（不会顺便导入子文件夹下的内容）
 
 func main() {
 	gomod.PrintAboutMessage()
